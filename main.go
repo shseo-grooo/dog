36c035dcf4de8e0ad4ad38c6443b9dc832728cce
shseo-grooo <shseo@grooo.co.jp>
2022-03-10 16:45:08 +0900

    v8

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 		mode := append(c.Request.Header["X-Mode"], "ACTIVE")[0]
 		url := c.Request.Host + c.Request.URL.Path
-		c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("[Dog Server v7.9 - %s from %s] %s", mode, url, callCat(mode))})
+		c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("[Dog Server v8 - %s from %s] %s", mode, url, callCat(mode))})
 	})
 
 	r.Run()
